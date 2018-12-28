a20f01eb6e87c26cbdfaee7eb4e3aeef00d21880
彭定林 <1979945716@qq.com>
2018-12-29 00:12:23 +0800

    category接口添加跨域

--- src/main/java/com/pdl/blog/controller/CategoryController.java
+++ src/main/java/com/pdl/blog/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.pdl.blog.service.CategoryService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/category")
 public class CategoryController {
 
